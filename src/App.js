6185a0f53179dad890938a2291f43bcfd7779d17
[Abhinavtongale] <abhitongale0123@gmail.com>
2022-12-20 00:02:36 +0530

    map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 function App() {
   let title = "Map Demo";
-  let user = { id: 1, city: "mumbai" };
+  let list = ["mumbai", "delhi"];
 
   return (
     <div>
       <h1> {title} </h1>
-      <h1>{user.city}</h1>
+
+      {list.map((item) => (
+        <h1>{item}</h1>
+      ))}
     </div>
   );
 }
