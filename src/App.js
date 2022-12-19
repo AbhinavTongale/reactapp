490134cdf1b47573cacbec8c543f135206fbda89
[Abhinavtongale] <abhitongale0123@gmail.com>
2022-12-20 00:01:37 +0530

    b8

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
-import { useState } from "react";
-
 function App() {
-  let str1 = "home";
-  let str2 = "Exploer";
-  let str3 = "Notification";
+  let title = "Map Demo";
+  let user = { id: 1, city: "mumbai" };
 
-  let [counter, setcounter] = useState(1);
-  let [changeCity] = useState("mumbai");
-  let changeCity = () =>{
-    city = "hello"+ city;
-    setCity (city);
-
-  };
-  let increment = () => {
-
-    counter++;
-    console.log(counter);
-
-    setcounter(counter);
-
-  };
   return (
     <div>
-      <h1>{city}</h1>
-      <input type="button" value="changecity" onClick={changeCity}/>
-      <hr />
-      <h1>{str1}</h1>
-      <h1>{str2}</h1>
-      <h1>{str3}</h1>
-      <h1>{counter}</h1>
-      <input type="button" value="increment" onClick={increment}/>
-      </div>
+      <h1> {title} </h1>
+      <h1>{user.city}</h1>
+    </div>
   );
 }
 
 export default App;
-
-    </div>
-  )
-
-}
