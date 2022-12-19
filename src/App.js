14640560d847eae1139105ae4425ba5a4f886855
[Abhinavtongale] <abhitongale0123@gmail.com>
2022-12-19 23:17:12 +0530

    interpolation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  let project = "My React Project";
+  let username = "Abhinavvv ";
+
+  // JSX => HTML CSS
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> {project} </h1>
+      <h1>Hello$ {username} $</h1>
     </div>
   );
 }
